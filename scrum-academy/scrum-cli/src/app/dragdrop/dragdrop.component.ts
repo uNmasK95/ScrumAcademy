2ef852fab9896290c4c43593be00ca3df7db2daf
Rafa Mota <Rafa Mota>
2017-05-01 11:39:06 +0100

    Add some stufs

---
+++ scrum-academy/scrum-cli/src/app/dragdrop/dragdrop.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { DragulaService } from 'ng2-dragula';
+
+@Component({
+  selector: 'app-dragdrop',
+  templateUrl: './dragdrop.component.html',
+  styleUrls: ['./dragdrop.component.css']
+})
+export class DragdropComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
