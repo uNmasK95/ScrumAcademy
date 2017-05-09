9ffde70a0c790e5cd9d925eb58e2f8d52f2ccb21
Rui Freitas <rui4825@gmail.com>
2017-05-09 17:13:14 +0100

    allow acess

--- scrum-academy/scrum-api/app/controllers/application_controller.rb
+++ scrum-academy/scrum-api/app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::API
 
   # called before every action on controllers
   before_action :authorize_request
+  after_action :set_access_control_headers
   attr_reader :current_user
 
   private
@@ -12,5 +13,11 @@ class ApplicationController < ActionController::API
   # Check for valid request token and return user
   def authorize_request
     @current_user = (AuthorizeApiRequest.new(request.headers).call)[:user]
+  end
+
+  # This is used to allow the cross origin POST requests made by confroom kiosk app.
+  def set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = "*"
+    headers['Access-Control-Request-Method'] = %w{GET POST OPTIONS}.join(",")
   end
 end
