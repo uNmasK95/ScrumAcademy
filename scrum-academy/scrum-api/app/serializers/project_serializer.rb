648301275b612405dc4ae92772197e582dec6d30
Rui Freitas <rui4825@gmail.com>
2017-05-22 15:06:03 +0100

    c

--- scrum-academy/scrum-api/app/serializers/project_serializer.rb
+++ scrum-academy/scrum-api/app/serializers/project_serializer.rb
@@ -2,7 +2,7 @@ class ProjectSerializer < ActiveModel::Serializer
   attributes :id, :name, :description, :startDate, :endDate
 
   belongs_to :team
-  has_many :team_user, through: :team
+  #has_many :team_users, through: :team
 
   belongs_to :statement  
 end
